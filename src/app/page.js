637c685ea5ad464562abf254f8fb29d46bef9d08
Rona4cs3 <132717094+Rona4cs3@users.noreply.github.com>
2024-12-02 21:53:06 +0000

    Pequenos testes

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,8 @@ export default function Home() {
       <main>
         <h1>Room Preview</h1>
         <p>A room preview app :0</p>
-        <input type="text" placeholder="Search Room"></input>
+        <input type="text" placeholder="Search Room" className="search"></input>
+        <button className="reserve">New reserve</button>        
       </main>
     </div>
   );
