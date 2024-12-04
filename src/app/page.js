088d551c79775163c7bc7f3fb990416ff17d162f
Rona4cs3 <132717094+Rona4cs3@users.noreply.github.com>
2024-12-04 00:43:14 +0000

    updade_css_reserve_page

--- src/app/page.js
+++ src/app/page.js
@@ -3,22 +3,36 @@ export default function Home() {
   return (
     <div className="bodycontainer">      
       <main>
-        <div className="corpo">
+        <div className="topcontainer">
           <div className="top">            
             <h1>Reservas</h1>
             <button className="reserve">+</button>
           </div>
-          <input type="text" placeholder="Search Room" className="search"></input>          
+          <div className="inputcontainer">
+            <img src="src/app/assets/header_images/search.png" alt="Search Icon"></img>
+            <input type="text" placeholder="Search Room" className="search"></input>          
+          </div>
         </div>
         <div className="roomcontainer">
           <div className="room">
-            <p>Sala</p>
-            <p>Pessoa</p>
-            <p>Data</p>
-            <p>Horário</p>            
+            <p className="info1">Sala</p>
+            <p className="info2">Pessoa</p>
+            <p className="info3">Data</p>
+            <p className="info4">Horário</p>            
             <button className="moreinfo"><img src="src/app/assets/sidebar_images/more.png" alt="More Info"/></button>
           </div>
         </div>
+        {/*<div className="pages_count">
+          <div className="count">
+            <p className="atual">1</p>
+            <p>-</p>
+            <p className="total">5</p>
+          </div>
+          <div className="arrows">
+            <img src="" alt="left"></img>
+            <img src="" alt="right"></img>
+          </div>
+        </div>*/}
       </main>
     </div>    
   );
