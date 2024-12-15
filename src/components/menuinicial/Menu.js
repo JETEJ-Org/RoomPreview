59e79b9dd5ebd48f813eee4c00fdc7d75f4758f0
John Vitor <johnvitoralves39@gmail.com>
2024-12-15 20:51:25 -0300

    Remove unused components and clean up routing

--- src/components/menuinicial/Menu.js
+++ src/components/menuinicial/Menu.js
@@ -5,12 +5,6 @@ const Menu = () => {
   return (
     <div className="divLinks">
       <Link className="linkNav" to="/reserva">Reserva</Link>
-      <Link className="linkNav" to="/login">Login</Link>
-      <Link className="linkNav" to="/sidebar">Sidebar</Link>
-      <Link className="linkNav" to="/building">Building</Link>
-      <Link className="linkNav" to="/start">Start</Link>
-      <Link className="linkNav" to="/cadastro">Cadastro</Link>
-      <Link className="linkNav" to="/splashscreen">SplashScreen</Link>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
+import "./global.css";
 
 // Routes
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Start from "./routes/Start/start";
 import Home from "./routes/Home/Home";
-import Login from "./routes/Login/Login";
-import Building from "./routes/Building/Building";
-import Cadastro from "./routes/Register/Register";
 import Reserva from "./routes/Reserva/Reserva";
-// Filhos de Login
-import LoginPage from "./components/loginpage/loginPage";
-import RecuperarSenha from "./components/loginpage/recuperarSenha";
-import RecuperarSenhaPage from "./components/loginpage/recuperarSenhaPage";
-import DefinirNovaSenha from "./components/loginpage/definirNovaSenha";
-
-// Components
-import Sidebar from "./components/sidebar/sidebar";
-import Loading from "./components/loading/loading";
 
 // Config Routes
 const router = createBrowserRouter([
@@ -27,52 +15,8 @@ const router = createBrowserRouter([
     element: <Home />,
   },
   {
-  path: "/reserva",
-  element: <Reserva />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-    children: [
-      {
-        index: true,
-        element: <LoginPage />,
-      },
-      {
-        path: 'recuperar_senha',
-        element: <RecuperarSenha />,
-        children: [
-          {
-            index: true,
-            element: <RecuperarSenhaPage />,
-          },
-          {
-            path: 'nova_senha',
-            element: <DefinirNovaSenha />,
-          }
-        ],
-      },
-    ],
-  },
-  {
-    path: "/cadastro",
-    element: <Cadastro />,
-  },
-  {
-    path: "/sidebar",
-    element: <Sidebar />,
-  },
-  {
-    path: "/building",
-    element: <Building />,
-  },
-  {
-    path: "/start",
-    element: <Start />,
-  },
-  {
-    path: "/splashscreen",
-    element: <Loading />,
+    path: "/reserva",
+    element: <Reserva />,
   }
 ]);
 
